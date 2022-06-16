fac332a6b17c09b45e8f0ff885ff59e7aab06219
antheboets <anthe.boets@student.ehb.be>
2022-06-16 17:58:51 +0200

    changing profile timer

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,11 @@ import Profile from "./Profile";
 
 const Header = ({profileData}) => {
 
-
     return (<div>
         <h1>Anthe Boets</h1>
-        <Profile profileData={profileData} seconds={10}/>
+        <Profile profileData={profileData} seconds={2}/>
         <HeaderItem />
     </div>);
 };
 
-export default Header;
+export default Header
