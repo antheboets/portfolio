0553647bfd22b0338178fd66d8207465bc503cf8
antheboets <anthe.boets@student.ehb.be>
2022-06-22 00:04:33 +0200

    removing tokens

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import React from 'react';
-import tokens from './tokens.js';
 import Data from './Data.js';
 
 function  App() {
