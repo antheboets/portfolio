24811026fd0745d6b6047a62c41f5325b823d023
antheboets <anthe.boets@student.ehb.be>
2022-06-24 02:15:34 +0200

    adding react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
